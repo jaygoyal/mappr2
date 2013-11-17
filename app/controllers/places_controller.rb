5e149849caae1e702714faa35c6ea523073c8410
Jay Goyal <jaygoyal@Jay-Goyals-MacBook-Pro.local>
2013-11-17 15:56:27 -0500

    Add forum for the place and other stuff

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,23 @@ class PlacesController < ApplicationController
 	def index
 		@place = Place.last
 	end
+
+	def new
+
+		@place = Place.new
+
+	end	
+
+	def create
+		@place = Place.create( place_params )
+		redirect_to root_path #this directs the input to the correct place
+					# You can find the correct location through the command rake routes in the terminal window
+	end	
+
+	private
+
+	def place_params
+		params.require(:place).permit( :title, :address)
+
+	end
 end
