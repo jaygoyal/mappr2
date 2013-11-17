ccde1f717ea5e22934f6ddd8fbcf33a220dc4b5a
Jay Goyal <jaygoyal@Jay-Goyals-MacBook-Pro.local>
2013-11-17 14:34:28 -0500

    Setting up for the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr2::Application.config.secret_key_base = 'f7ad97aad6f4d9c3aa0185df31b3a3ba4fd7c4b13253e02e05853f20acf77c403804e23d2868e3b62462543388278bea33070f7d1b2ef3779dfee18ed4d17753'
